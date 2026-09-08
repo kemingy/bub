2a71c9edebc525e4204f378c5e037c70fb03f168
Keming <kemingy94@gmail.com>
2026-09-08 22:03:01 +0800

    fix: link verbosity to logging level

--- tests/test_cli_help.py
+++ tests/test_cli_help.py
@@ -24,7 +24,7 @@ def test_cli_instrumentation_disables_local_variable_diagnostics(monkeypatch: py
     monkeypatch.setattr(logger, "remove", lambda: None)
     monkeypatch.setattr(logger, "add", lambda _sink, **options: sink_options.append(options))
 
-    _instrument_bub()
+    _instrument_bub(level="INFO")
 
     assert sink_options
     assert all(options.get("diagnose") is False for options in sink_options)

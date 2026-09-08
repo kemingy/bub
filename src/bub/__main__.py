b066800d09c2f878cf55357bd8189beb2a33ef74
Keming <kemingy94@gmail.com>
2026-09-08 23:00:21 +0800

    fix: link verbosity to logging level (#299)

--- src/bub/__main__.py
+++ src/bub/__main__.py
@@ -6,28 +6,35 @@ import sys
 
 import typer
 
+from bub.builtin.settings import load_settings
 from bub.framework import BubFramework
 
+LOG_LEVEL = {
+    0: "INFO",
+    1: "DEBUG",
+    2: "TRACE",
+}
 
-def _instrument_bub() -> None:
+
+def _instrument_bub(level: str) -> None:
     from loguru import logger
 
     logger.remove()
-    logger.add(sys.stderr, colorize=True, diagnose=False)
+    logger.add(sys.stderr, level=level, colorize=True, diagnose=False)
 
     try:
         import logfire
         from logfire.integrations.loguru import LogfireHandler
 
         logfire.configure()
-        logger.add(LogfireHandler(), format="{message}", diagnose=False)
+        logger.add(LogfireHandler(), level=level, format="{message}", diagnose=False)
     except Exception as exc:
         logger.debug("logfire instrumentation disabled: {}", exc)
 
 
 def create_cli_app() -> typer.Typer:
-    _instrument_bub()
     framework = BubFramework()
+    _instrument_bub(LOG_LEVEL[load_settings().verbose])
     framework.load_hooks()
     app = framework.create_cli_app()
 
